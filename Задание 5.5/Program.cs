88e477cf7527baf035fe6da97368c4799ba7e9af
Александр Грибов <aleksgribov@inbox.ru>
2021-11-29 17:25:11 +0300

    Добавьте файлы проекта.

---
+++ Задание 5.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5._5
+{// Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN следующего вида:
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк");
+            int n = (int)double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов");
+            int m = (int)double.Parse(Console.ReadLine());
+            int[,] t = new int[n, m];
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < m; j++)
+                {
+                    t[i, j] = random.Next(0, 2);
+
+                    Console.Write(" {0,4} ", t[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ Задание 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{ // Формирование одномерного массива из 7 элементов. Заполнить массив числами, вводимыми с клавиатуры, определить среднее арифмитическое элекментов.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+
+            double Sred = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Введите {0} число массива", i+1);
+                
+                array[i] = (int) double.Parse(Console.ReadLine());
+                Sred = Sred + array[i];
+               
+            }
+            double S = Sred / array.Length;
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" " + array[i]);
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифмитическое {0} ",S);
+            
+            Console.ReadKey();
+        }
+    }
+}
